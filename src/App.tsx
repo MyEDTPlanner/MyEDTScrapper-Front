e05a93ff1e81e40c0adca0b6ec0655cd93d58bbf
Grégoire Lichou <22329645+glichou@users.noreply.github.com>
2022-10-28 14:20:37 +0200

    :recycle: Mise en place de React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FC } from 'react';
+import Calendar from './components/calendar'
+import './index.css'
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
