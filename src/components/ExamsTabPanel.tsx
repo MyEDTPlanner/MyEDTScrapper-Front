c05d4f1c8a903a505a96827fe7f3a3e2b3aeb5cf
MEDANE Aghiles <77809263+aghiles-medane@users.noreply.github.com>
2023-02-05 00:55:56 +0100

    :sparkles: Affichage de la date et heures des examens

--- src/components/ExamsTabPanel.tsx
+++ src/components/ExamsTabPanel.tsx
@@ -1,53 +1,62 @@
-import * as React from 'react';
+import * as React from "react";
 import {
-    Box,
-    Typography,
-    List,
-    ListItem,
-    ListItemAvatar,
-    Avatar,
-    ListItemText,
-    Divider,
-    Alert,
-    AlertTitle
-} from '@mui/material';
-import { EventInterface } from '../models/EventInterface';
-import { formatAttendeeName, formatTitleAbbreviation } from '../utils/format';
+  Box,
+  Typography,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Divider,
+  Alert,
+  AlertTitle,
+} from "@mui/material";
+import { EventInterface } from "../models/EventInterface";
+import { formatAttendeeName, formatTitleAbbreviation } from "../utils/format";
 
 interface Props {
-    events: EventInterface[];
+  events: EventInterface[];
 }
 
-export const ExamsTabPanel = ({events}: Props) => {
-    return (
-        <Box sx={{ p: 3 }}>
-            <Typography>Examens</Typography>
-            <List dense={true}>
-              {getEventExams(events).map((e) => (
-                <ListItem key={e.uuid}>
-                    <ListItemAvatar>
-                        <Avatar>
-                            {formatTitleAbbreviation(e.title)}
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={e.title}
-                        secondary={e.locations.join(', ') + ' - ' + e.attendees.map(name => formatAttendeeName(name)).join(', ')}
-                    />
-                </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
-}
+export const ExamsTabPanel = ({ events }: Props) => {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography>Examens</Typography>
+      <List dense={true}>
+        {getEventExams(events).map((e) => (
+          <ListItem key={e.uuid}>
+            <ListItemAvatar>
+              <Avatar>{formatTitleAbbreviation(e.title)}</Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={e.title}
+              secondary={
+                `${new Date(e.start).toLocaleDateString()} - ${new Date(
+                  e.start
+                ).toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })} -` +
+                e.locations.join(", ") +
+                " - " +
+                e.attendees.map((name) => formatAttendeeName(name)).join(", ")
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
 
 const getEventExams = (events: EventInterface[]) => {
-    console.log(events);
-    events = events.filter(event => 
-        event.type === "Examen"
-        //&& event.start
-        //&& event.start > new Date()
-    );
-    console.log(events);
-    return events;
-}
+  console.log(events);
+  events = events.filter(
+    (event) =>
+      event.type === "Examen" &&
+      event.start &&
+      new Date(event.start) > new Date()
+  );
+  console.log(events);
+  return events;
+};
--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,83 +1,88 @@
 import {
-    AppBar,
-    InputAdornment,
-    Toolbar,
-    Typography,
-    Autocomplete,
-    TextField
+  AppBar,
+  InputAdornment,
+  Toolbar,
+  Typography,
+  Autocomplete,
+  TextField,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled } from "@mui/material/styles";
 
 type Props = {
-    groups: any[];
-    selectedGroup: any;
-    handleGroupChange: (event: React.SyntheticEvent, value: any | Array<any>, reason: string) => void;
-}; 
+  groups: any[];
+  selectedGroup: any;
+  handleGroupChange: (
+    event: React.SyntheticEvent,
+    value: any | Array<any>,
+    reason: string
+  ) => void;
+};
 
-export const Topbar = ({groups, selectedGroup, handleGroupChange}: Props) => {
-    return (
-        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-            Agenda
-          </Typography>
-          <Autocomplete
-            disablePortal
-            options={groups}
-            getOptionLabel={(option) => option.name}
-            sx={{ width: 300 }}
-            noOptionsText="Aucun groupe correspondant"
-            onChange={handleGroupChange}
-            value={selectedGroup || null}
-            isOptionEqualToValue={(option, value) => option.code === value.code}
-            renderInput={(params) => 
-              <StyledTextField 
-                {...params}
-                placeholder="Groupe..."
-                size="small"
-                InputProps={{
-                  ...params.InputProps,
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SearchIcon
-                        style={{ color: "white", marginLeft: "8px" }}
-                      />
-                    </InputAdornment>
-                  )
-                }}
-              />
-            }
-          />
-        </Toolbar>
-      </AppBar>
-    );
+export const Topbar = ({ groups, selectedGroup, handleGroupChange }: Props) => {
+  return (
+    <AppBar
+      position="fixed"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <Toolbar>
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          Agenda
+        </Typography>
+        <Autocomplete
+          disablePortal
+          options={groups}
+          getOptionLabel={(option) => option.name}
+          sx={{ width: 300 }}
+          noOptionsText="Aucun groupe correspondant"
+          onChange={handleGroupChange}
+          value={selectedGroup || null}
+          isOptionEqualToValue={(option, value) => option.code === value.code}
+          renderInput={(params) => (
+            <StyledTextField
+              {...params}
+              placeholder="Groupe..."
+              size="small"
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon style={{ color: "white", marginLeft: "8px" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+        />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 const StyledTextField = styled(TextField)(({ theme }) => ({
-    color: "#fff",
-    background: "rgba(255, 255, 255, 0.15)",
-    borderRadius: "4px",
-    width: "100%",
-    "& input": {
-      color: "#fff !important"
-    },
-    "& fieldset": {
+  color: "#fff",
+  background: "rgba(255, 255, 255, 0.15)",
+  borderRadius: "4px",
+  width: "100%",
+  "& input": {
+    color: "#fff !important",
+  },
+  "& fieldset": {
+    borderWidth: "0px",
+    "& fieldset:hover, & fieldset:focus, & fieldset:active": {
       borderWidth: "0px",
-      "& fieldset:hover, & fieldset:focus, & fieldset:active": {
-        borderWidth: "0px"
+    },
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(2, 1, 1, 2),
+      transition: theme.transitions.create("width"),
+      color: "#fff",
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
       },
-      "& .MuiInputBase-input": {
-        padding: theme.spacing(2, 1, 1, 2),
-        transition: theme.transitions.create("width"),
-        color: "#fff",
-        width: "100%",
-        [theme.breakpoints.up("sm")]: {
-          width: "12ch",
-          "&:focus": {
-            width: "20ch"
-          }
-        }
-      }
-    }
+    },
+  },
 }));
